206bc83fbe3b641530016f5c2d900c85a270323e
Cora <jguo33@g.ucla.edu>
2025-06-26 23:28:29 +0000

    testingYOLO

--- cora.py
+++ cora.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
-model.train(data="path/to/dataset.yaml", epochs=100, imgsz=640)
+model.train(data="env/lib/python3.10/site-packages/ultralytics/cfg/datasets/coco8.yaml", epochs=100, imgsz=640)
