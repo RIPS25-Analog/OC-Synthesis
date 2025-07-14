18e445e3185c3bc5e83395733f7543b55ec10031
Vikhyat <vikhyat2603@gmail.com>
2025-07-14 03:04:32 +0000

    narrow down hyperparam opt; clean files;

--- src/evaluate_YOLO.py
+++ src/evaluate_YOLO.py
@@ -4,8 +4,10 @@ import yaml
 import argparse
 
 class YOLOevaluator:
-    def __init__(self, run_path=None, data_path=None, data_split='val', model=None):
+    def __init__(self, run_path=None, data_path=None, data_split='val', model=None, eval_img_size=640, classes=None):
         self.run_path = run_path
+        self.eval_img_size = eval_img_size
+        self.classes = classes
 
         if model is None:
             assert run_path is not None, "If no model is provided, run_path must be specified."
@@ -13,7 +15,7 @@ class YOLOevaluator:
             self.model = YOLO(self.model_path, task='detect')
             self.data_path = data_path if data_path else self._get_data_path_from_model_yaml()
         else:
-            self.model = model
+            self.model = YOLO(model, task='detect')
             assert data_path is not None, "If a model is provided, data_path must also be specified."
             self.data_path = data_path
 
@@ -36,7 +38,8 @@ class YOLOevaluator:
     def evaluate_model(self):
         """Evaluate the YOLO model and return results."""
         project_name = 'runs/' + self.data_path.split('/')[-1].split('.')[0]
-        results = self.model.val(data=self.data_path, project=project_name, split=self.data_split)
+        results = self.model.val(data=self.data_path, project=project_name, split=self.data_split,
+                                  imgsz=self.eval_img_size, classes=self.classes,)
         print(f"Evaluating on dataset: {self.data_path} with split: {self.data_split}")
         
         # Print the evaluation results
@@ -70,9 +73,16 @@ if __name__ == "__main__":
     parser.add_argument('--run_path', type=str, default='yolo11n.pt', help='Path to the YOLO run directory (to fetch best weights from).')
     parser.add_argument('--data_path', type=str, required=False, help='Path to the dataset configuration file (YAML). (Optional, will be fetched from the run YAML if not provided).')
     parser.add_argument('--data_split', type=str, default='val', choices=['train', 'val', 'test'], help='Specify the dataset split to evaluate on (default: val).')
+    parser.add_argument('--model', type=str, default=None, help='Path to the YOLO model file (optional). If provided, data_path must also be specified.')
+    parser.add_argument('--eval_img_size', type=int, default=640, help='Image size for evaluation (default: 640).')
+    parser.add_argument('--classes', type=str, default=None, help='Comma-separated list of class names to evaluate (default: None, evaluates all classes).')
 
     args = parser.parse_args()
 
-    evaluator = YOLOevaluator(args.run_path, args.data_path, args.data_split)
+    args.classes = list(map(int, args.classes.split(',') if args.classes else None))
+
+    evaluator = YOLOevaluator(args.run_path, args.data_path,
+                            args.data_split, args.model,
+                            args.eval_img_size, args.classes)
     results = evaluator.evaluate_model()
     print(results)  # Print the evaluation results
--- src/hyperparam_opt_YOLO.py
+++ src/hyperparam_opt_YOLO.py
@@ -12,18 +12,18 @@ def train_with_wandb(config=None):
         config = wandb.config
     
         train_kwargs = {
-            'lr0': config.lr0,
-            'lrf': config.lrf,
-            'momentum': config.momentum,
-            'weight_decay': config.weight_decay,
-            'warmup_epochs': config.warmup_epochs,
-            'warmup_momentum': config.warmup_momentum,
-            'warmup_bias_lr': config.warmup_bias_lr,
-            'box': config.box,
-            'cls': config.cls,
-            'dfl': config.dfl,
+            # 'lr0': config.lr0,
+            # 'lrf': config.lrf,
+            # 'momentum': config.momentum,
+            # 'weight_decay': config.weight_decay,
+            # 'warmup_epochs': config.warmup_epochs,
+            # 'warmup_momentum': config.warmup_momentum,
+            # 'warmup_bias_lr': config.warmup_bias_lr,
+            # 'box': config.box,
+            # 'cls': config.cls,
+            # 'dfl': config.dfl,
             # 'nbs': config.nbs,
-            'dropout': config.dropout,
+            # 'dropout': config.dropout,
             'batch': config.batch,
             'imgsz': config.imgsz,
             'epochs': config.epochs,
@@ -37,7 +37,14 @@ def train_with_wandb(config=None):
             **train_kwargs
         )
         
-        val_results = finetuner.train_model() # model evaluated on some arbitrary validation set
+        train_val_results = finetuner.train_model() # model evaluated on some arbitrary validation set
+        
+        evaluator = YOLOevaluator(model=finetuner.model,
+                                  data_path=config.data_path,
+                                  data_split='val',
+                                  eval_img_size=config.eval_img_size)  # Evaluate on validation set
+        
+        val_results = evaluator.evaluate_model()  # Evaluate the model
         
         del finetuner # maybe clear memory?
 
@@ -55,35 +62,38 @@ def run_hyperparameter_optimization(project_name, data_path, model_path, sweep_c
     
     # Create sweep configuration
     sweep_config = {
-        'method': 'bayes',  # Can be 'grid', 'random', or 'bayes'
+        'method': 'random',  # Can be 'grid', 'random', or 'bayes'
         'metric': {'name': 'mAP50', 'goal': 'maximize'},
         'parameters': {
             # Learning rate parameters
-            'lr0': {'distribution': 'log_uniform_values', 'min': 0.0001, 'max': 0.1},
-            'lrf': {'distribution': 'uniform', 'min': 0.001, 'max': 0.1},
-            'momentum': {'distribution': 'uniform', 'min': 0.85, 'max': 0.95},
-            'weight_decay': {'distribution': 'log_uniform_values', 'min': 0.0001, 'max': 0.001},
+            # 'lr0': {'distribution': 'log_uniform_values', 'min': 0.0001, 'max': 0.1},
+            # 'lrf': {'distribution': 'uniform', 'min': 0.001, 'max': 0.1},
+            # 'momentum': {'distribution': 'uniform', 'min': 0.85, 'max': 0.95},
+            # 'weight_decay': {'distribution': 'log_uniform_values', 'min': 0.0001, 'max': 0.001},
 
-            # Warmup parameters
-            'warmup_epochs': {'distribution': 'uniform', 'min': 1.0, 'max': 5.0},
-            'warmup_momentum': {'distribution': 'uniform', 'min': 0.5, 'max': 0.9},
-            'warmup_bias_lr': {'distribution': 'uniform', 'min': 0.05, 'max': 0.2},
+            # # Warmup parameters
+            # 'warmup_epochs': {'distribution': 'uniform', 'min': 1.0, 'max': 5.0},
+            # 'warmup_momentum': {'distribution': 'uniform', 'min': 0.5, 'max': 0.9},
+            # 'warmup_bias_lr': {'distribution': 'uniform', 'min': 0.05, 'max': 0.2},
 
-            # Loss function weights
-            'box': {'distribution': 'uniform', 'min': 5.0, 'max': 10.0},
-            'cls': {'distribution': 'uniform', 'min': 0.25, 'max': 1.0},
-            'dfl': {'distribution': 'uniform', 'min': 1.0, 'max': 2.0},
+            # # Loss function weights
+            # 'box': {'distribution': 'uniform', 'min': 5.0, 'max': 10.0},
+            # 'cls': {'distribution': 'uniform', 'min': 0.25, 'max': 1.0},
+            # 'dfl': {'distribution': 'uniform', 'min': 1.0, 'max': 2.0},
 
-            # Regularization
-            'dropout': {'distribution': 'uniform', 'min': 0.0, 'max': 0.5},
+            # # Regularization
+            # 'dropout': {'distribution': 'uniform', 'min': 0.0, 'max': 0.5},
             
             # Training parameters
-            'batch': {'values': [16, 24, 32]},
-            'imgsz': {'values': [320, 480, 640, 800]},
+            'batch': {'values': [32]},
+            'imgsz': {'values': [640, 800, 960, 1120]},
+            'eval_img_size': {'values': [800, 960, 1280, 1600]},
+            'multi_scale': {'values': [False, True]},
+
             # 'nbs': {'values': [32, 64, 128]},
             
             # Architecture parameters
-            'freeze': {'distribution': 'int_uniform', 'min': 20, 'max': 23},
+            'freeze': {'values': [8, 12, 16]}#{'distribution': 'int_uniform', 'min': 10, 'max': 20},
         }
     }
     
@@ -114,8 +124,6 @@ if __name__ == "__main__":
                        help='Path to the YOLO model file.')
     parser.add_argument('--sweep_count', type=int, default=50, 
                        help='Number of hyperparameter combinations to try.')
-    parser.add_argument('--metric_name', type=str, default='final_mAP50',
-                       help='Metric to optimize (final_mAP50, final_mAP50-95).')
     parser.add_argument('--epochs', type=int, default=20,
                        help='Number of epochs for training in each hyperparameter run.')
     
